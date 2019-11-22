942a418fe5cf9e8b6b4641df5a56a1ad49d93933
tmouhamed <s.toqaa@gmail.com>
2019-11-22 16:37:10 -0500

    updated

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -7,6 +7,11 @@ class Hero extends React.Component {
     render() {
         return (
             <>
+
+            <div>
+                <h4>Unfold. The Possibilities.</h4>
+                <p className="test">Gain more control over adopting</p><p className="test"> digital technology and unfold infinite possibilities to </p><p className="test">grow your business</p>
+            </div>
                 <section className="main__hero">
                     <Map />
                     <img className="main__hero-img" src={Image} alt="image"></img>
