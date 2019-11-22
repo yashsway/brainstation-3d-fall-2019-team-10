149bce923fe60ab6462e517b8010fa010a46f11d
Luca Lamanna <luca.lamanna@hotmail.com>
2019-11-22 13:28:12 -0500

    add more files

---
+++ client/src/components/ProvinceContent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class ProvinceContent extends Component {
+    render() {
+
+        return (
+            <div className='main-container'>
+                
+                
+            </div>
+        )
+    }
+}
